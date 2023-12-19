7e441e225f7bd45548750e08a4a84ced177b7d9b
QentangifyCodes <keshavrshah@gmail.com>
2023-12-19 16:13:40 -0500

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  mode:"development",
+  entry: {
+    main: path.resolve(__dirname, './src/main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].js'
+  }
+}
